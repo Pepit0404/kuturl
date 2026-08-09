b327ec648920edcee9940389da078c0e686a156c
Pepit0 <mainpepito@gmail.com>
2026-08-09 20:32:34 +0200

    migrate to fetch and fix empty page on error

--- web/public/script/script.js
+++ web/public/script/script.js
@@ -6,79 +6,69 @@ document.addEventListener("keydown", (event) => {
   }
 });
 
-function createShort() {
+async function createShort() {
   let original = document.getElementById("origin").value;
   let shorter = document.getElementById("short").value;
 
-  let xhr = new XMLHttpRequest();
-  xhr.open("POST", `${BASE_API}/url`, true);
-  xhr.setRequestHeader("Content-Type", "application/json; charset=utf-8");
+  const url = `${BASE_API}/url`;
 
-  xhr.onreadystatechange = function () {
-    if (xhr.readyState === 4) {
-      try {
-        const response = JSON.parse(xhr.responseText);
-        if (xhr.status === 201) {
-          CreateNotification(
-            "success",
-            "Link created",
-            "Your short link has been created successfully.",
-            ["Close", "Copy"],
-            `${document.URL}${response.result.shorter_url}`,
-          );
-          document.getElementById("origin").value = "";
-          document.getElementById("short").value = "";
-        } else {
-          CreateNotification(
-            "warning",
-            "Warning",
-            response.message,
-            ["Close"],
-            `${document.URL}`,
-          );
-        }
-      } catch (error) {
-        CreateNotification(
-          "warning",
-          "Warning",
-          "An error has occured",
-          ["Close"],
-          `${document.URL}`,
-        );
-        console.error("No api response", error);
-      }
-    }
-  };
+  const response = await fetch(url, {
+    method: "POST",
+    body: JSON.stringify({ original_url: original, shorter_url: shorter }),
+    headers: {
+      "Content-Type": "application/json; charset=utf-8",
+    },
+  });
+  const result = await response.json();
 
-  // Send data as key-value pairs
-  let data = JSON.stringify({ original_url: original, shorter_url: shorter });
-  xhr.send(data);
+  if (!response.ok) {
+    CreateNotification(
+      "warning",
+      "Warning",
+      result.message,
+      ["Close"],
+      `${document.URL}`,
+    );
+    throw new Error("No api response", error);
+  }
+
+  console.log(result);
+  if (result.status === "success") {
+    CreateNotification(
+      "success",
+      "Link created",
+      "Your short link has been created successfully.",
+      ["Close", "Copy"],
+      `${document.URL}${result.result.shorter_url}`,
+    );
+    document.getElementById("origin").value = "";
+    document.getElementById("short").value = "";
+  }
 }
 
-function redirectTo() {
+async function redirectTo() {
   const shorter = document.URL.split("/").slice(3, 4).join("/");
   const baseUrl = document.URL.split("/").slice(0, 3).join("/");
   if (shorter === "") {
     console.error("No shorter URL found");
     return;
   }
-  let xhr = new XMLHttpRequest();
-  xhr.open("GET", `${BASE_API}/url/${shorter}`, true);
-  xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
 
-  xhr.onreadystatechange = function () {
-    if (xhr.readyState === 4 && xhr.status === 200) {
-      const response = JSON.parse(xhr.responseText);
-      const success = response.status === "success";
-      if (success) {
-        window.location.href = response.result.original_url;
-      } else {
-        window.location.href = baseUrl;
-      }
+  const url = `${BASE_API}/url/${shorter}`;
+  const response = await fetch(url);
+
+  if (!response.ok) {
+    if (response.status === 404) {
+      window.location.href = baseUrl;
     }
-  };
+    throw new Error(`Response status: ${response.status}`);
+  }
+  const result = await response.json();
 
-  xhr.send();
+  const success = result.status === "success";
+  if (success) {
+    window.location.href = response.result.original_url;
+  }
 }
 
 // https://www.youtube.com/watch?v=dQw4w9WgXcQ
